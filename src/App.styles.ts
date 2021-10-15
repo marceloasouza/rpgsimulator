b4e629bdfd1b5d5a66ee3965631d7f4aa1139054
marceloasouza <marceloamorsouza@yahoo.com.br>
2021-10-15 11:14:49 -0300

    link img

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -9,8 +9,7 @@ export const Container = styled.div`
 export const Map = styled.div`
   width: 480px;
   height: 480px;
-  background-color: url('https://drive.google.com/file/d/1o2EdDdsZ06F-JcuFNIstwZgWsZDw-wo7/view?usp=sharing');
-  //background-image: url('/assets/map.png');
+  background-image: url('/assets/map.png');
   background-position: left top;
   background-size: 100%;
 `;
